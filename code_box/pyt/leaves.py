9252a6941b50497dd45a7461ea138093287264b5
gestos <master@identitaetsmanagement.net>
2016-12-30 18:23:25 +0100

    updated

--- code_box/pyt/leaves.py
+++ code_box/pyt/leaves.py
@@ -2,10 +2,10 @@
 import sys
 import os
 
-print sys.argv[1]
-toplevel = sys.argv[1]
-print (toplevel)
+print sys.argv[1]           # this is the first command line argument
+toplevel = sys.argv[1]      # from now on called toplevel
+print (toplevel)            # as we see, it's the same
 
-for dirpath, dirnames, filenames in os.walk(toplevel):
-    if not dirnames:
-        print dirpath
+for aa, bb, cc in os.walk(toplevel):  # the python for does not need a termination like do<->done in bash
+    if not bb:                                    # no dirnames=leaf node
+        print aa
