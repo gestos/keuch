8d126bc1ceb63975f1dc191424d0be7951efd687
gestos <master@identitaetsmanagement.net>
2017-01-15 05:34:22 +0100

    write_xls

--- code_box/pyt/spreadsheetparsing/parseit.py
+++ code_box/pyt/spreadsheetparsing/parseit.py
@@ -7,19 +7,23 @@ import sys
 from natsort import natsorted, ns
 import xlwt
 from xlutils import copy as xlcopy
+import colorama
 
-print len(sys.argv)
+grn = colorama.Fore.GREEN
+blu = colorama.Fore.BLUE
+cya = colorama.Fore.CYAN
+red = colorama.Fore.RED
+rst = colorama.Style.RESET_ALL
+
 
 if len(sys.argv) > 1:
-#    print "there's a command line argument"
     daily_report_dir_path = os.path.abspath(sys.argv[1])
 else:
     daily_report_dir_path = '/home/keuch/gits/wiki_carex/stats/Hotlineber1458Gesing_taegl_217_HOURLY/'
-    
-print (daily_report_dir_path)
+print (grn + "Directory with .xls files is set to:\t" + rst + daily_report_dir_path)
+
 
 liste_aller_gelesenen_files = list()
-
 for tabelle in natsorted(os.listdir(daily_report_dir_path), alg=ns.IGNORECASE, reverse=True):
     if tabelle.endswith(".xls"):
         tabelle_fullpath = os.path.join(daily_report_dir_path, tabelle)
@@ -31,44 +35,34 @@ for tabelle in natsorted(os.listdir(daily_report_dir_path), alg=ns.IGNORECASE, r
         Date_usable = Datum_korrekt.encode("utf-8")
         datensatz_fuer_dieses_sheet = list()
 
-#        print(zeilen)
-#        print (tabelle_fullpath)
-#        print (Datum_korrekt)
-
         datensatz_fuer_dieses_sheet.append(Date_usable)
 
         wanted_cols = [2,3,4,5,12] # this is a comment 2=alle anrufe 3=telefonierte anrufe 4=gesamtanrufzeit 5=gesamtverbindungszeit 12=gesamtNBzeit
         for Spalte in wanted_cols:
             summe = int()
-#            print ("{}: {}".format("Spalte", Spalte))
             for summenreihen in range(4,28):
                 summand = erstes_sheet.cell(summenreihen,Spalte).value
-                # print (summand)
                 summe = summe+summand
-#            print (summe)
             datensatz_fuer_dieses_sheet.append(summe)
-#        print(datensatz_fuer_dieses_sheet)
         liste_aller_gelesenen_files.append(datensatz_fuer_dieses_sheet)
-#        print
-    
-
 
 #### END READER ####
+
 
 #### BEGIN WRITER ####
 
 targetfile = '/home/keuch/gits/keuch/code_box/pyt/spreadsheetparsing/target.xls'
 target_workbook = xlrd.open_workbook(targetfile)
 sheets_in_targetfile = target_workbook.sheet_names()
-print (sheets_in_targetfile)
 targetsheet = target_workbook.sheet_by_index(0)
 schon_befueelt = targetsheet.nrows
-print(schon_befueelt)
-print (liste_aller_gelesenen_files[0]) # This shows that the list from above is still populated and available even outside the for loop
-
-
 row_to_write_in = schon_befueelt +1
-print (row_to_write_in)
+print (grn + "Zieldatei hat folgende Sheets:\t\t" + rst + ",".join(sheets_in_targetfile))
+print(grn + "vorhandene Zeilen in Zieldatei:\t\t" + rst + str(schon_befueelt) + ", ab " + str(row_to_write_in) + " wird weitergeschrieben")
+print (grn + "Datenformat:\t\t\t\t" + rst + ", ".join(str(p) for p in liste_aller_gelesenen_files[0])) # This shows that the list from above is still populated and available even outside the for loop
+
+
+
 
 target_workbook_writeable = xlcopy.copy(target_workbook)
 sheet_rw = target_workbook_writeable.get_sheet(0)
