506b3d6e3ee49be2d4a1a173b94d57f9db0978e2
gestos <master@identitaetsmanagement.net>
2017-01-16 04:24:15 +0100

    functions

--- code_box/pyt/spreadsheetparsing/parseit.py
+++ code_box/pyt/spreadsheetparsing/parseit.py
@@ -7,44 +7,47 @@ import sys
 from natsort import natsorted, ns
 import xlwt
 from xlutils import copy as xlcopy
-import colorama
+from colorama import Fore as coly, Style as coln
 
-grn = colorama.Fore.GREEN
-blu = colorama.Fore.BLUE
-cya = colorama.Fore.CYAN
-red = colorama.Fore.RED
-rst = colorama.Style.RESET_ALL
+grn = coly.GREEN
+blu = coly.BLUE
+cya = coly.CYAN
+red = coly.RED
+rst = coln.RESET_ALL
 
+def check_xls_path():
+    if len(sys.argv) > 1:
+        xlspath = os.path.abspath(sys.argv[1])
+    else:
+        xlspath = '/home/keuch/gits/wiki_carex/stats/Hotlineber1458Gesing_taegl_217_HOURLY/'
+    print (grn + "Directory with .xls files is set to:\t" + rst + xlspath)
+    return xlspath
 
-if len(sys.argv) > 1:
-    daily_report_dir_path = os.path.abspath(sys.argv[1])
-else:
-    daily_report_dir_path = '/home/keuch/gits/wiki_carex/stats/Hotlineber1458Gesing_taegl_217_HOURLY/'
-print (grn + "Directory with .xls files is set to:\t" + rst + daily_report_dir_path)
+daily_report_dir_path = check_xls_path()
 
-
-liste_aller_gelesenen_files = list()
-for tabelle in natsorted(os.listdir(daily_report_dir_path), alg=ns.IGNORECASE, reverse=True):
-    if tabelle.endswith(".xls"):
-        tabelle_fullpath = os.path.join(daily_report_dir_path, tabelle)
-        ganze_tabelle = xlrd.open_workbook(tabelle_fullpath)
-        erstes_sheet = ganze_tabelle.sheet_by_index(0)
-        zeilen = erstes_sheet.nrows
-        Datum_B2 = erstes_sheet.cell(1,1).value
-        Datum_korrekt = re.sub(r' ', '.', Datum_B2[:10]) 
-        Date_usable = Datum_korrekt.encode("utf-8")
-        datensatz_fuer_dieses_sheet = list()
-
-        datensatz_fuer_dieses_sheet.append(Date_usable)
-
-        wanted_cols = [2,3,4,5,12] # this is a comment 2=alle anrufe 3=telefonierte anrufe 4=gesamtanrufzeit 5=gesamtverbindungszeit 12=gesamtNBzeit
-        for Spalte in wanted_cols:
-            summe = int()
-            for summenreihen in range(4,28):
-                summand = erstes_sheet.cell(summenreihen,Spalte).value
-                summe = summe+summand
-            datensatz_fuer_dieses_sheet.append(summe)
-        liste_aller_gelesenen_files.append(datensatz_fuer_dieses_sheet)
+def read_data_from_whole_dir():
+    datensaetze_aller_files = list()
+    for tabelle in natsorted(os.listdir(daily_report_dir_path), alg=ns.IGNORECASE, reverse=True):
+        if tabelle.endswith(".xls"):
+            tabelle_fullpath = os.path.join(daily_report_dir_path, tabelle)
+            ganze_tabelle = xlrd.open_workbook(tabelle_fullpath)
+            erstes_sheet = ganze_tabelle.sheet_by_index(0)
+            zeilen = erstes_sheet.nrows
+            Datum_B2 = erstes_sheet.cell(1,1).value
+            Datum_korrekt = re.sub(r' ', '.', Datum_B2[:10]) 
+            Date_usable = Datum_korrekt.encode("utf-8")
+            wanted_cols = [2,3,4,5,12] # this is a comment 2=alle anrufe 3=telefonierte anrufe 4=gesamtanrufzeit 5=gesamtverbindungszeit 12=gesamtNBzeit
+            datensatz_fuer_dieses_sheet = list()
+            
+            datensatz_fuer_dieses_sheet.append(Date_usable)
+            for Spalte in wanted_cols:
+                summe = int()
+                for summenreihen in range(4,28):
+                    summand = erstes_sheet.cell(summenreihen,Spalte).value
+                    summe = summe+summand
+                datensatz_fuer_dieses_sheet.append(summe)
+            datensaetze_aller_files.append(datensatz_fuer_dieses_sheet)
+            return datensaetze_aller_files
 
 #### END READER ####
 
@@ -57,15 +60,13 @@ sheets_in_targetfile = target_workbook.sheet_names()
 targetsheet = target_workbook.sheet_by_index(0)
 schon_befueelt = targetsheet.nrows
 row_to_write_in = schon_befueelt +1
+target_workbook_writeable = xlcopy.copy(target_workbook)
+sheet_rw = target_workbook_writeable.get_sheet(0)
+liste_aller_gelesenen_files = read_data_from_whole_dir()
+
 print (grn + "Zieldatei hat folgende Sheets:\t\t" + rst + ",".join(sheets_in_targetfile))
 print(grn + "vorhandene Zeilen in Zieldatei:\t\t" + rst + str(schon_befueelt) + ", ab " + str(row_to_write_in) + " wird weitergeschrieben")
 print (grn + "Datenformat:\t\t\t\t" + rst + ", ".join(str(p) for p in liste_aller_gelesenen_files[0])) # This shows that the list from above is still populated and available even outside the for loop
-
-
-
-
-target_workbook_writeable = xlcopy.copy(target_workbook)
-sheet_rw = target_workbook_writeable.get_sheet(0)
 
 for einzelner_datensatz in liste_aller_gelesenen_files:
     idx = 0
