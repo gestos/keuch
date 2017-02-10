946c30930bdcf00085f62aff644b4c426456778a
gestos <master@identitaetsmangement.eu>
2017-02-10 19:44:17 +0100

    nearly there

--- code_box/pyt/spreadsheetparsing/parseit2.py
+++ code_box/pyt/spreadsheetparsing/parseit2.py
@@ -6,6 +6,7 @@ import re
 import sys
 from natsort import natsorted, ns
 import xlwt
+from xlwt import Formula
 from xlutils import copy as xlcopy
 from colorama import Fore as coly, Style as coln
 from datetime import datetime
@@ -44,7 +45,6 @@ def check_cmdline_params():
         print("target:\t" + targetfile)
         return xlspath, targetfile
 # global variables for later use
-xlspath, targetfile = check_cmdline_params()              # check for matching input / output files
 neue_global = {}    # a dictionary of all files that aren't present in the target spreadsheet yet
 neue_daten = []     # a list, only used for checking duplicates on the fly in the read_single_to_dict() function
 dupes_global = {}       # a dictionary that contains list of files for duplicate dates
@@ -52,7 +52,10 @@ dupes_filelist = []     # a list of duplicate files that is referenced by the da
 agenten = {}
 agenten_daten = []
 other_global = {}       # a dictionary that contains files that aren't daily reports
+
+xlspath, targetfile = check_cmdline_params()              # check for matching input / output files
 wholdir = natsorted(os.listdir(xlspath), alg=ns.IGNORECASE, reverse=True)  # the list of all files in the specified directory
+
 def parsedate(daily_sheet): # turn crap date into nice date
     date_crap = daily_sheet.cell(1,1).value # comes like this from upstream, date always lives at 1,1
     date_clea = re.sub(r' ', '.', date_crap[:10]) # transform to a string that strptime can parse
@@ -156,8 +159,9 @@ targetsheet = target_workbook.sheet_by_index(0)
 sheet_rw = target_workbook_writeable.get_sheet(0)
 start_writing = targetsheet.nrows
 new_entries_by_date = sorted(neue_global, key=lambda k: neue_global[k][0]) #this actually sorts by the first entry of the list (k)'s first item, which is the date integer
-style_datum = xlwt.easyxf(num_format_str = "nn, dd.mm.yy")
-style_stunden = xlwt.easyxf(num_format_str = "HH:MM:SS")
+style_zahl_int = xlwt.easyxf('alignment: horiz centre')
+style_datum = xlwt.easyxf('alignment: horiz centre; borders: right medium', num_format_str = "nn, dd.mm.yy")
+style_stunden = xlwt.easyxf('alignment: horiz centre', num_format_str = "HH:MM:SS")
 
 print(grn + "vorhandene Zeilen in Zieldatei:\t\t" + rst + str(start_writing-1) + ", ab " + str(start_writing) + " wird weitergeschrieben")
 
@@ -167,24 +171,34 @@ def write_out(startrow):
         print("ok; bye :-)")
         exit()
     else:
-        for i in new_entries_by_date:
-            print(neue_global[i])
-            idx = 0
-            for col in neue_global[i]:
-                if idx == 0:    #0 ist das Datum wanted_cols = [3,21,5,12] # 3=telefonierte anrufe, 21=verlorene, 5=gesamtverbindungszeit 12=gesamtNBzeit
-                    sheet_rw.write(startrow, idx, col, style_datum)
-                elif idx in (1, 2): # 1,2 = spalten 3, 21 im urpsrungssheet = telefonierte und verlorene. Format sollte Integer sein
-                    sheet_rw.write(startrow, idx, col)
-                elif idx in (3, 4): # 3,4 = spalten 5, 12 im ursprunssheet = gesamtVBzeit und gesamtNBzeit. Format sollte HH:MM:SS sein (es geht jeweils nur um einen Tag, 24 HH sind ausreichend)
-                    sheet_rw.write(startrow, idx, col, style_stunden)
-                idx += 1
+        for row in new_entries_by_date:
+            # idx = 0
+            # for col in neue_global[row]:    # go through each column of the data set
+            #     if idx == 0:    #0 ist das Datum wanted_cols = [3,21,5,12] # 3=telefonierte anrufe, 21=verlorene, 5=gesamtverbindungszeit 12=gesamtNBzeit
+            #         sheet_rw.write(startrow, idx, col, style_datum)
+            #     elif idx in (1, 2): # 1,2 = spalten 3, 21 im urpsrungssheet = telefonierte und verlorene. Format sollte Integer sein
+            #         sheet_rw.write(startrow, idx, col)
+            #     elif idx in (3, 4): # 3,4 = spalten 5, 12 im ursprunssheet = gesamtVBzeit und gesamtNBzeit. Format sollte HH:MM:SS sein (es geht jeweils nur um einen Tag, 24 HH sind ausreichend)
+            #         sheet_rw.write(startrow, idx, col, style_stunden)
+            #     idx += 1
+            dat = neue_global[row][0] # Datum
+            tel = neue_global[row][1] # Telefoniert
+            ver = neue_global[row][2] # Verloren
+            ges = neue_global[row][3] # Gespraechszeit
+            nac = neue_global[row][4] # Nacharbeitszeit
+            tot = ges+nac # Gesamtzeit
+
+            sheet_rw.write(startrow, 0, dat, style_datum)   #Datum
+            sheet_rw.write(startrow, 1, tel, style_zahl_int)    #Calls
+            sheet_rw.write(startrow, 2, Formula("IF(%s=0,0,%s/%s)" % (tel, tot, tel)), style_stunden) 
+            sheet_rw.write(startrow, 3, Formula("IF(%s=0,0,%s/%s)" % (tel, ges, tel)), style_stunden)
+            sheet_rw.write(startrow, 4, Formula("IF(%s=0,0,%s/%s)" % (tel, nac, tel)), style_stunden)
             startrow += 1
-        #target_workbook_writeable.save(targetfile)
-        print(dir(xlrd))
-        print(targetsheet.cell(0,0).xf_index)
-        print(targetsheet.cell(0,0).ctype)
-        print(targetsheet.cell(0,0).dump)
-        print("data written to " + targetfile)
+        target_workbook_writeable.save(targetfile)
+        # print(dir(xlrd))
+        # print(targetsheet.cell(0,0).ctype)
+        # print(targetsheet.cell(0,0).dump)
+        # print("data written to " + targetfile)
 
 write_out(start_writing)
 # xlrd can't get the values of formula cells because those are only created when the file was saved (with "recalculate" option) in excel (or LO)
