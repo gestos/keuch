d1524d8ba100571a02443a0331ba663e4d35da97
gestos <master@identitaetsmanagement.net>
2017-03-04 13:09:16 +0100

    new approach for daily sheets

--- code_box/pyt/spreadsheetparsing/parse_agents_daily.py
+++ code_box/pyt/spreadsheetparsing/parse_agents_daily.py
@@ -7,22 +7,31 @@ from colorama import Fore as coly, Style as coln
 from datetime import datetime,date
 
 
+def get_cw_ma(ma_file, agent_id_set):
+    ma_cell = xlrd.open_workbook(ma_file, formatting_info=True).sheet_by_index(0).cell(0,1).value
+    ma_list = ma_cell.split(',')
+    teile = re.compile(r'(^\D)\s(\D*)(\d*$)')
+    for i in ma_list:
+        try:
+            standort = teile.match(i).group(1).strip()
+            kuerzel = teile.match(i).group(2).strip()
+            id_num = int(teile.match(i).group(3).strip())
+            print ("Agent: " + str(kuerzel) + " ist am Standort " + str(standort) + " und hat die Nummer " + str(id_num))
+            agent_id_set[id_num] = dict()
+            agent_id_set[id_num]["standort"] = standort
+            agent_id_set[id_num]["kuerzel"] = kuerzel
+        except:
+            print("sth wrong with " + str(i))
+    return agent_id_set
 
-def get_cw_ma(ma_file):
-    sheet = xlrd.open_workbook(ma_file, formatting_info=True).sheet_by_index(0)
-    srow = 2
-    erow = sheet.nrows
-    col = 2
-    mitarbeiter = dict()
-    for row in range(srow, erow):
-        if sheet.cell(row,2).value:
-            print type(sheet.cell(row,2).value)
-            print int(sheet.cell(row,2).value)
-            int(sheet.cell(row,2).value)
-        
-        
 ##########################################################################
 ##############START OF PROGRAM############################################
 ##########################################################################
 
-get_cw_ma(sys.argv[1])
+agent_ids=dict()
+
+agent_ids = get_cw_ma(sys.argv[1], agent_ids)
+print agent_ids
+
+agent_ids = get_cw_ma(sys.argv[2], agent_ids)
+print agent_ids.items()
