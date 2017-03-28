840fadc0cbcd826ad14685c25afa6a9d612f41ad
gestos <master@identitaetsmangement.eu>
2017-03-28 19:17:39 +0200

    tables update 2703

--- code_box/pyt/spreadsheetparsing/auslastung_nach_stunden.py
+++ code_box/pyt/spreadsheetparsing/auslastung_nach_stunden.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import os, csv, math, xlrd, re, sys, xlwt, calendar, textwrap
+import os, csv, math, xlrd, re, sys, xlwt, calendar, textwrap, pandas, numpy
 from natsort import natsorted, ns
 from xlwt import Formula
 from xlutils import copy as xlcopy
@@ -95,8 +95,7 @@ def write_out(in_dict,target_file):
     sheet_sla_rw                = target_workbook_writeable.get_sheet(2)
 
     style_datum             = xlwt.easyxf('alignment: horiz right; borders: right double, right_color 0x28, left double, left_color 0x28', num_format_str = "ddd, dd.mm.yy")
-    style_conn           = xlwt.easyxf('alignment: horiz centre; pattern: pattern solid, fore_color 0x2c')
-    style_n_conn           = xlwt.easyxf('alignment: horiz centre; pattern: pattern solid, fore_color 0x1f')
+    style_zahl           = xlwt.easyxf('alignment: horiz centre; pattern: pattern solid')
     style_sla               = xlwt.easyxf('alignment: horiz centre;', num_format_str = "0.00")
 
     sheet_verbunden_rw.write(row_verbunden, 0, tag, style_datum)   #Datum
@@ -106,12 +105,13 @@ def write_out(in_dict,target_file):
     sheet_verloren_rw.write(row_verloren, 1, kw)   #Datum
     sheet_sla_rw.write(row_sla, 1, kw)   #Datum
     for index in in_dict:   # der Stunden_index faengt bei 0 an und ist analog zu den Spalten im Sheet
+        print (str(index) + " wird in spalte " + str(index+2) + " geschrieben")
     
         conn  = in_dict[index]['verbunden']
-        sheet_verbunden_rw.write(row_verbunden,index+2, conn, style_conn)
+        sheet_verbunden_rw.write(row_verbunden,index+2, conn, style_zahl)
 
         n_conn  = in_dict[index]['verloren']
-        sheet_verloren_rw.write(row_verloren,index+2,n_conn, style_n_conn)
+        sheet_verloren_rw.write(row_verloren,index+2,n_conn, style_zahl)
     
         sla  = in_dict[index]['servicelevel']
         sheet_sla_rw.write(row_sla,index+2,sla, style_sla)
@@ -124,3 +124,9 @@ source,target = check_cmdline_params()
 tag,kw,stunden = generate_single_day(source)
 
 write_out(stunden,target)
+
+h_df = pandas.DataFrame.from_dict(stunden)
+print h_df
+h_df['summe']=h_df.sum(axis=1)
+vlo = h_df.loc("verloren")
+print vlo
