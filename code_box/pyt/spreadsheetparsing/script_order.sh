447ce06f8f443544e211e29e3795bf098b6794c1
gestos <master@identitaetsmanagement.net>
2017-11-03 13:29:07 +0100

    stats und plots 0111

--- code_box/pyt/spreadsheetparsing/script_order.sh
+++ code_box/pyt/spreadsheetparsing/script_order.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+bpp='/home/keuch/gits/keuch/code_box/pyt/spreadsheetparsing'
+echo ${base_parse_path}
 
-./rename_only.py test_stats/
-cp test_stats/*xls test_stats/archiv/
-./hotline_daily1458.py test_stats/archiv/ hotline_daily1458.xls
-./hotline_daily1459.py test_stats/archiv/ hotline_daily1459.xls
-./kws_agenten.py test_stats/archiv/ kws_agenten.xls
-./hotline_to_pickle1458.py test_stats/archiv/ pkl_plots_calls_1458.pkl
-./hotline_to_pickle1459.py test_stats/archiv/ pkl_plots_calls_1459.pkl
+${bpp}/rename_only.py ${bpp}/test_stats/
+cp ${bpp}/test_stats/*xls ${bpp}/test_stats/archiv/
+${bpp}/hotline_daily1458.py ${bpp}/test_stats/archiv/ ${bpp}/hotline_daily1458.xls
+${bpp}/hotline_daily1459.py ${bpp}/test_stats/archiv/ ${bpp}/hotline_daily1459.xls
+${bpp}/kws_agenten.py ${bpp}/test_stats/archiv/ ${bpp}/kws_agenten.xls
+${bpp}/hotline_to_pickle1458.py ${bpp}/test_stats/archiv/ ${bpp}/pkl_plots_calls_1458.pkl
+${bpp}/hotline_to_pickle1459.py ${bpp}/test_stats/archiv/ ${bpp}/pkl_plots_calls_1459.pkl
