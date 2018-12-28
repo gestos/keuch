f6ef1cbf98a6b038f12df2caae119370bcacf9b2
gestos <master@identitaetsmanagement.net>
2018-12-28 21:02:25 +0100

    etikettendruck

--- liquidator/liqrechner/etiketten.js
+++ liquidator/liqrechner/etiketten.js
@@ -8,20 +8,16 @@ function shoot() {
 	var a3 = dgby('ar3').value || '-';
 	var a4 = dgby('ar4').value || '-';
 	var a5 = dgby('ar5').value || '-';
-	var a1p = dgby('aroma1pct').value || '0'+"%";
-	var a2p = dgby('aroma2pct').value || '0'+"%";
-	var a3p = dgby('aroma3pct').value || '0'+"%";
-	var a4p = dgby('aroma4pct').value || '0'+"%";
-	var a5p = dgby('aroma5pct').value || '0'+"%";
+	var a1p = dgby('aroma1pct').value +"%" || '0'+"%";
+	var a2p = dgby('aroma2pct').value +"%" || '0'+"%";
+	var a3p = dgby('aroma3pct').value +"%" || '0'+"%";
+	var a4p = dgby('aroma4pct').value +"%" || '0'+"%";
+	var a5p = dgby('aroma5pct').value +"%" || '0'+"%";
 	var flv_liq = String(dgby('arpct_5').value) + "%";
 	var nic_liq = String(dgby('nic_5').value) + " mg";
 	var pvg_liq = Number.parseFloat(dgby('pg_5').value).toFixed(1) + "/" + Number.parseFloat(dgby('vg_5').value).toFixed(1) + "%";
 	var todai = new Date();
 	var todate = todai.getDate()+'.'+todai.getMonth()+'.'+todai.getFullYear()
-	console.log(todate);
-	console.log(a1,a1p);
-
-
 
 	labeldiv = dgby('etikettenliste');
 	newtab = document.createElement('table');
@@ -33,9 +29,8 @@ function shoot() {
 	acll1.innerHTML=a1;
 	apcll1 = tr1.insertCell();
 	apcll1.innerHTML=a1p;
-	trenn = tr1.insertCell();
-	trenn.className="trenner";
-	trenn.rowSpan = 5;
+	trenn1 = tr1.insertCell();
+	trenn1.className="trenner";
 	argesamt = tr1.insertCell();
 	argesamt.innerHTML="Aroma/Liq";
 	argesamtpct = tr1.insertCell();
@@ -47,6 +42,8 @@ function shoot() {
 	acll2.innerHTML=a2;
 	apcll2 = tr2.insertCell();
 	apcll2.innerHTML=a2p;
+	trenn2 = tr2.insertCell();
+	trenn2.className="trenner";
 	pvgcll = tr2.insertCell();
 	pvgcll.innerHTML="PG/VG";
 	pvpct = tr2.insertCell();
@@ -58,6 +55,8 @@ function shoot() {
 	acll3.innerHTML=a3;
 	apcll3 = tr3.insertCell();
 	apcll3.innerHTML=a3p;
+	trenn3 = tr3.insertCell();
+	trenn3.className="trenner";
 	nicll = tr3.insertCell();
 	nicll.innerHTML="Nic";
 	nicpcll = tr3.insertCell();
@@ -69,6 +68,8 @@ function shoot() {
 	acll4.innerHTML=a4;
 	apcll4 = tr4.insertCell();
 	apcll4.innerHTML=a4p;
+	trenn4 = tr4.insertCell();
+	trenn4.className="trenner";
 	datcll = tr4.insertCell();
 	datcll.innerHTML="Datum";
 	datvcll = tr4.insertCell();
@@ -80,6 +81,9 @@ function shoot() {
 	acll5.innerHTML=a5;
 	apcll5 = tr5.insertCell();
 	apcll5.innerHTML=a5p;
+	trenn5 = tr5.insertCell();
+	trenn5.className="trenner";
+
 	removal = tr5.insertCell();
 	rembutton = document.createElement("input");
 	rembutton.className="pushbuttonr";
@@ -87,15 +91,22 @@ function shoot() {
 	rembutton.id="remover";
 	rembutton.value="Etikett loeschen";
 	rembutton.onclick=function(){
-		console.log(this);
-		console.log(this.parentElement);
-		console.log(this.parentElement.parentElement);
-		console.log(this.parentElement.parentElement.parentElement);
-		console.log(this.parentElement.parentElement.parentElement.parentElement);
 		this.parentElement.parentElement.parentElement.parentElement.remove();
 		return false;
 	}
 	removal.appendChild(rembutton);
 
 	labeldiv.appendChild(newtab);
+}
+
+
+function print(tabelle) {
+	var printwin = window.open('');
+	printwin.document.write('<html><head><title>Etikettendruck</title>');
+	printwin.document.write('<link rel="stylesheet" href="druck.css">');
+	printwin.document.write('</head><body>');
+	printwin.document.write(tabelle.outerHTML);
+	printwin.document.write('</body></html>');
+	printwin.document.close();
+	return true;
 }
